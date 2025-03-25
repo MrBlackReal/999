a0c695314a8d2a3c77f67936392dba979ab7a5e5
MrBlackReal <MrBlackReal@users.noreply.github.com>
2025-03-26 00:41:19 +0100

    Began with the project
    Added tts api
    Added base for voice commands

---
+++ src/index.js
@@ -0,0 +1,95 @@
+const { Client, Intents, IntentsBitField, GatewayIntentBits } = require('discord.js');
+const { joinVoiceChannel, VoiceConnectionStatus, getVoiceConnection, createAudioPlayer, createAudioResource, AudioPlayerStatus } = require('@discordjs/voice');
+const { addSpeechEvent, SpeechEvents } = require('discord-speech-recognition');
+const { TextToSpeechClient } = require('@google-cloud/text-to-speech');
+const fs = require("fs");
+const path = require('path');
+const axios = require("axios")
+
+require("dotenv").config()
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates] });
+
+const api_endpoint = "http://192.168.10.124:5000"
+const api_synthesize = api_endpoint + "/synthesize"
+const api_voices = api_endpoint + "/voices"
+
+if (!fs.existsSync(path.join(__dirname, "bin")))
+    fs.mkdir(path.join(__dirname, "bin"), () => { });
+
+addSpeechEvent(client, { lang: "de-DE" })
+
+client.on("error", err => console.error(err));
+
+client.once('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on(SpeechEvents.speech, async (msg) => {
+    if (!msg.content)
+        return;
+
+    console.log(`${msg.author.globalName}:`, msg.content);
+
+    if (msg.content.toLowerCase().includes("hallo wie geht's") || true) {
+        const conn = msg.connection;
+
+        if (!conn) {
+            console.log("invalid connection");
+            return;
+        }
+
+        const request = {
+            text: msg.content, //"Mir geht es gut. Wie geht es dir?",
+            voice: "de",
+        };
+
+        let response = null;
+        await axios.post(api_synthesize, request, {responseType: "arraybuffer"}).then(res => response = res.data).catch(err => console.error(err));
+
+        if (!response) {
+            console.log("resp is null");
+            return;
+        }
+
+        const fileName = Date.now().toString() + ".wav";
+        const loc = path.join(__dirname, "bin", fileName);
+
+        fs.writeFile(loc, Buffer.from(response), 'binary', err => err ?? console.error(err));
+        console.log('Audio content written to file: ' + loc);
+
+        const player = createAudioPlayer();
+
+        player.on("error", err => err ?? console.error("Error while playing audio:\n", err));
+        player.once(AudioPlayerStatus.Idle, () => fs.rm(loc, () => { }));
+
+        conn.subscribe(player);
+
+        const resource = createAudioResource(loc)
+        player.play(resource);
+    }
+});
+
+client.on('voiceStateUpdate', (oldState, newState) => {
+    if (newState.member.id == "377862824564621312") {
+        const channel = newState.channel;
+
+        if (!channel) {
+            const conn = getVoiceConnection(oldState.guild.id)
+
+            if (conn)
+                conn.destroy();
+
+            return
+        }
+
+        const conn = joinVoiceChannel({
+            channelId: channel.id,
+            guildId: channel.guildId,
+            adapterCreator: channel.guild.voiceAdapterCreator,
+            selfDeaf: false
+        });
+    }
+});
+
+client.login(process.env.BOT_TOKEN);
---
+++ tts/main.py
@@ -0,0 +1,49 @@
+import os
+import time
+import espeakng
+
+from flask import Flask, request, jsonify
+
+server = Flask(__name__)
+tts = espeakng.Speaker(voice="en-us")
+
+bin_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "bin")
+
+if not os.path.exists(bin_path):
+    os.mkdir(bin_path)
+
+@server.route("/synthesize", methods=["POST"])
+def synthesize():
+    data = request.get_json()
+    print(data)
+
+    text = data.get("text", None)
+
+    if not text:
+        return jsonify({"error": "No text was provided"}), 400
+
+    tts.voice = data.get("voice", "en-us")
+    tts.pitch = int(data.get("pitch", 50))
+    tts.wpm = int(data.get("speed", 175))
+
+    output_path = os.path.join(bin_path, f"{int(time.time())}.wav")
+
+    tts.say(text, export_path=output_path)
+
+    while not os.path.exists(output_path):
+        time.sleep(0.1)
+
+    with open(output_path, "rb") as audio:
+        audio_data = audio.read()
+        audio.close()
+
+    os.remove(output_path)
+
+    return audio_data, 200, {"Content-Type": "audio/wav"}
+
+@server.route("/voices", methods=["POST"])
+def voices():
+    return jsonify(tts.list_voices()), 200
+
+if __name__ == '__main__':
+    server.run(host='0.0.0.0', port=5000)
